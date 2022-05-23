9f54b6c7fc9e86913aa9638d3ce6f76802b07ad1
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-23 12:20:26 +0300

    array_8 consisted of 1 & 0

---
+++ task00/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int [] array = new int [8];
+FillArray (array);
+void FillArray(int[] a)
+{
+    int Length = a.Length;
+    int index=0;
+    while (index<Length)
+    {
+        a[index]=new Random().Next(0,2);
+        index++;
+    }
+}
+PrintArray (array);
+void PrintArray(int[] b)
+{
+    int len1 = b.Length;
+    int index1=0;
+    while (index1<len1)
+    {
+        Console.Write(b[index1]+" ");
+        index1++;
+    }
+}
